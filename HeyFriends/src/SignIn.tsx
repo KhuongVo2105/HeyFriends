48b92468003c14b1fbc30c743f6c7cf378be8290
KhuongVo2105 <khuongvo2105@gmail.com>
2025-03-03 22:55:00 +0700

    - Không cho đẩy các thư mục build tạm thời của ứng dụng lên repository - Cài đặt thư viện NativeWind v4 vào trong dự án - thành công

---
+++ HeyFriends/src/SignIn.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+            <View className="w-full bg-red-100">
+                <Text className='text-red-500 text-3xl'>Hello</Text>
+            </View>
+        </View>
+    )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({})
